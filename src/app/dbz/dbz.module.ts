e905bbcbd0e86dd1a05707dbb9ac6cc46e3c5d0e
Ivan <imatamala93@gmail.com>
2023-09-18 21:08:53 -0300

    segunda patita modulo 2

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {MainPageComponent} from "./pages/main-page.component";
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
+import {FormsModule} from "@angular/forms";
 
 
 
@@ -12,9 +13,10 @@ import { AddCharacterComponent } from './components/add-character/add-character.
     ListComponent,
     AddCharacterComponent
   ],
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
   exports: [
     MainPageComponent
   ]
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Character} from "../interfaces/character.interface";
+import {DbzService} from "../services/dbz.service";
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,18 +7,8 @@ import {Character} from "../interfaces/character.interface";
 })
 
 export class MainPageComponent {
-  constructor() {
-  }
+  constructor(public dbzService: DbzService) {
 
-  public characters: Character[] = [{
-    name: 'Krilin',
-    power: 1000
-  }, {
-     name: 'Goku',
-     power: 9500
-    },
-    {name: 'Vegeta',
-    power: 7500
-    }];
+  }
 
 }
